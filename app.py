51cc8cc656b428210707bdc60981787160ca25fc
awanbadut <diniazik2006@gmail.com>
2026-04-19 16:28:03 +0700

    zikry:

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>🚀 DevOps Hero - Pipeline Berhasil di update!</h1>'
+    return '<h1>🚀 DevOps Hero - Pipeline Berhasil di update!!</h1>'
 
 @app.route('/health')
 def health():
